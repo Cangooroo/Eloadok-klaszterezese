2e89351acc4e11b038d5338adbb53e72af75c686
Bence Szabó <bence.szabo.92@gmail.com>
2014-12-18 01:30:05 +0100

    README

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ import httplib2
 # Adatbázis model
 import model
 
+import numpy as np
+
+from scipy.cluster.vq import kmeans, vq
+
 
 # --------------- Globális változók a kényelem érdekében
 
 # default Last.fm usernév (user-ek, akiknek az adataival dolgozunk: molnarmarcell, csorvagep, Cangooroo)
-USER = 'Cangooroo'
+USERS = ['csorvagep', 'Cangooroo']
 
 BASE_URL = 'http://ws.audioscrobbler.com/2.0/'
 API_KEY = '39431f9f072f751be8f9477ba1411442'
@@ -33,7 +37,7 @@ def construct_url(parameters, base_url=BASE_URL):
     return '{base_url}?{parameters}'.format(base_url=BASE_URL, parameters=parameters)
 
 
-def get_topartists(http_client, user=USER, api_key=API_KEY, format=FORMAT, limit=LIMIT):
+def get_topartists(http_client, api_key=API_KEY, format=FORMAT, limit=LIMIT, user=''):
     """
     Lekérdezi a megadott user-től a top előadók listáját
     """
@@ -120,8 +124,44 @@ def get_toptags(http_client, artist, api_key=API_KEY, format=FORMAT):
     #       ...
     #       ...
     try:
-        toptags = [(tag.get('name'), tag.get('count')) for tag in
-                   json.loads(content.decode()).get('toptags').get('tag')]
+        toptags = []
+        for tag in json.loads(content.decode()).get('toptags').get('tag'):
+            name = tag.get('name').upper()
+
+            if(name in ['DNB', "DRUM'N'BASS", 'DRUM & BASS', 'DRUM N BASS',
+                        'LIQUID', 'LIQUID DRUM AND BASS', 'LIQUID FUNK']):
+                name = 'DRUM AND BASS'
+
+            if(name in ['PUNK', 'PUNK ROCK', 'POP PUNK', 'EMO']):
+                name = 'PUNK'
+
+            if(name in ['HIP HOP', 'UNDERGROUND HIP-HOP', 'JAZZ HOP', 'INSTRUMENTAL HIP-HOP'
+                        'TRIP-HOP', 'GLITCH', 'GLITCH HOP', 'GLITCH-HOP', 'AUSSIE HIP HOP']):
+                name = 'HIP-HOP'
+
+            if(name in ['GANGSTA', 'GANGSTA RAP', 'WEST COAST RAP', 'RAPCORE']):
+                name = 'RAP'
+
+            if(name in ['R&B', ]):
+                name = 'RNB'
+
+            if(name in ['NEUROFUNK', ]):
+                name = 'FUNK'
+
+            if(name in ['ELECTRONICA', ]):
+                name = 'ELECTRONIC'
+
+            if(name in ['ALTERNATIVE ROCK', 'ALTERNATIVE', 'INDIE ROCK', 'HARD ROCK',
+                        'CLASSIC ROCK', 'NU METAL']):
+                name = 'ROCK'
+
+            if(name in ['CHILLSTEP', ]):
+                name = 'DUBSTEP'
+
+            if int(tag.get('count')) > 50:
+                toptags.append((name, tag.get('count')))
+                print('{}({})'.format(name, tag.get('count')), end=', ')
+
     except Exception:
         # Exception általában abból adódik, hogy az előadónév nem jó (például Valaki ft. Valaki), ilyenkor pedig
         # a Last-fm adatbázisában nincs hozzá adat, a szerver nem tölti ki a megfelelő mezőket a json-ben, amikre
@@ -155,6 +195,163 @@ def save_tags(artist_name, tags, session):
         else:
             session.add(new_tag)
 
+
+def create_vectors(artist, session):
+    tags = session.query(model.Tag).filter(model.Tag.artist_id == artist.id)
+
+    ds = ['DRUM AND BASS',
+            'ROCK',
+            'PUNK',
+            'HIP-HOP',
+            'RAP',
+            'DUBSTEP',
+            'ELECTRONIC',
+            'INDIE',
+            'SOUL',
+            'POP',
+            'RNB',
+            'FUNK',
+        ]
+
+    printit = False
+
+    if tags.count()>0:
+        for tag in tags:
+            if tag.name in ds:
+                printit = True
+
+    if printit:
+        print(artist.name,end=': ')
+        artist_vector = model.ArtistVector(artist_id=artist.id)
+        artist_vector.drum_and_bass = 0
+        artist_vector.rock = 0
+        artist_vector.punk = 0
+        artist_vector.hip_hop = 0
+        artist_vector.rap = 0
+        artist_vector.dubstep = 0
+        artist_vector.electronic = 0
+        artist_vector.indie = 0
+        artist_vector.soul = 0
+        artist_vector.pop = 0
+        artist_vector.rnb = 0
+        artist_vector.funk = 0
+
+        for tag in tags:
+            if tag.name in ds:
+                print(tag.name, end=', ')
+                if tag.name == 'DRUM AND BASS':
+                    if artist_vector.drum_and_bass < tag.count:
+                        artist_vector.drum_and_bass = tag.count
+
+                if tag.name == 'ROCK':
+                    if (artist_vector.rock or 0) < tag.count:
+                        artist_vector.rock = tag.count
+
+                if tag.name == 'PUNK':
+                    if (artist_vector.punk or 0) < tag.count:
+                        artist_vector.punk = tag.count
+
+                if tag.name == 'HIP-HOP':
+                    if (artist_vector.hip_hop or 0) < tag.count:
+                        artist_vector.hip_hop = tag.count
+
+                if tag.name == 'RAP':
+                    if (artist_vector.rap or 0) < tag.count:
+                        artist_vector.rap = tag.count
+
+                if tag.name == 'DUBSTEP':
+                    if (artist_vector.dubstep or 0) < tag.count:
+                        artist_vector.dubstep = tag.count
+
+                if tag.name == 'ELECTRONIC':
+                    if (artist_vector.electronic or 0) < tag.count:
+                        artist_vector.electronic = tag.count
+
+                if tag.name == 'INDIE':
+                    if (artist_vector.indie or 0) < tag.count:
+                        artist_vector.indie = tag.count
+
+                if tag.name == 'SOUL':
+                    if (artist_vector.soul or 0) < tag.count:
+                        artist_vector.soul = tag.count
+
+                if tag.name == 'POP':
+                    if (artist_vector.pop or 0) < tag.count:
+                        artist_vector.pop = tag.count
+
+                if tag.name == 'RNB':
+                    if (artist_vector.rnb or 0) < tag.count:
+                        artist_vector.rnb = tag.count
+
+                if tag.name == 'FUNK':
+                    if (artist_vector.funk or 0) < tag.count:
+                        artist_vector.funk = tag.count
+        print()
+        session.add(artist_vector)
+
+
+def compute(session):
+        artist_vectors = session.query(model.ArtistVector).all()
+
+        in_raw_data = []
+
+        for artist_vector in artist_vectors:
+            in_raw_data.append(np.array([
+                artist_vector.drum_and_bass,
+                artist_vector.rock,
+                artist_vector.punk,
+                artist_vector.hip_hop,
+                artist_vector.rap,
+                artist_vector.dubstep,
+                artist_vector.electronic,
+                artist_vector.indie,
+                artist_vector.soul,
+                artist_vector.pop,
+                artist_vector.rnb,
+                artist_vector.nu_metal,
+                artist_vector.funk
+            ]))
+
+        data = np.vstack(in_raw_data)
+
+        centers, _ = kmeans(data, 26, iter=40)
+        clusters, _ = vq(data, centers)
+
+        for cluster in range(26):
+            for index, cluster2 in enumerate(clusters):
+                if cluster2 == cluster:
+                    try:
+                        artist_name = session.query(model.Artist).get(index+1)
+                        print(artist_name.name, '-', cluster2)
+                    except AttributeError:
+                        print(index+1, cluster2)
+
+
+def compute2(session):
+    artist_vectors = session.query(model.ArtistVector).all()
+
+    data = []
+
+    for artist_vector in artist_vectors:
+        data.append([
+                artist_vector.drum_and_bass,
+                artist_vector.rock,
+                artist_vector.punk,
+                artist_vector.hip_hop,
+                artist_vector.rap,
+                artist_vector.dubstep,
+                artist_vector.electronic,
+                artist_vector.indie,
+                artist_vector.soul,
+                artist_vector.pop,
+                artist_vector.rnb,
+                artist_vector.nu_metal,
+                artist_vector.funk
+            ])
+
+
+
+
 # -------------- Fő program
 if __name__ == '__main__':
 
@@ -168,19 +365,33 @@ if __name__ == '__main__':
     model.Base.metadata.create_all(engine)
     s = session()
 
-    # HTTP kliens példányosítása (a .cache könyvtárba cash-el)
-    http_client = httplib2.Http('.cache')
+    # # HTTP kliens példányosítása (a .cache könyvtárba cash-el)
+    # http_client = httplib2.Http('.cache')
+    #
+    # for user in USERS:
+    #     # Top előadók lekérdezése
+    #     artists = get_topartists(http_client, user=user)
+    #
+    #     # Előadók elmentése az adatbázisba
+    #     save_artists(artists, s)
+    #
+    #     for artist in artists:
+    #         print(artist, end=': ')
+    #         tags = get_toptags(http_client, artist)  # Előadónként top tag-ek lekérdezése
+    #         save_tags(artist, tags, s)
+    #         print()
+    #         # print(tags)
+    #
+    #
+    # s.commit()
+    #
+    # artists = s.query(model.Artist).all()
+    #
+    # for artist in artists:
+    #     create_vectors(artist, s)
+    #
+    # s.commit()
 
-    # Top előadók lekérdezése
-    artists = get_topartists(http_client)
-
-    # Előadók elmentése az adatbázisba
-    save_artists(artists, s)
-
-    for artist in artists:
-        print(artist, end=': ')
-        tags = get_toptags(http_client, artist)  # Előadónként top tag-ek lekérdezése
-        save_tags(artist, tags, s)
-        print(tags)
+    compute(s)
 
     s.commit()
--- model.py
+++ model.py
@@ -16,3 +16,22 @@ class Tag(Base):
     artist_id = Column(ForeignKey('artist.id'))
     name = Column(String)
     count = Column(Integer)
+
+
+class ArtistVector(Base):
+    __tablename__ = 'vector'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    artist_id = Column(ForeignKey('artist.id'))
+    drum_and_bass = Column(Integer, default=0)
+    rock = Column(Integer, default=0)
+    punk = Column(Integer, default=0)
+    hip_hop = Column(Integer, default=0)
+    rap = Column(Integer, default=0)
+    dubstep = Column(Integer, default=0)
+    electronic = Column(Integer, default=0)
+    indie = Column(Integer, default=0)
+    soul = Column(Integer, default=0)
+    pop = Column(Integer, default=0)
+    rnb = Column(Integer, default=0)
+    nu_metal = Column(Integer, default=0)
+    funk = Column(Integer, default=0)
